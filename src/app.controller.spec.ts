084424b2a9b7482a67f04f9626322567a7bc1a09
hs.lee@catenoid.net <hs.lee@catenoid.net>
2023-01-20 13:26:11 +0900

    github master action24

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//3
+//4
 describe('AppController', () => {
   let appController: AppController;
 
